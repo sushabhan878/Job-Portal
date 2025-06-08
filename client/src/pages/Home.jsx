59b7fc37c88fd3ee5cf82e99b31d2f2f94b0ac1f
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-08 11:22:50 +0530

    JobListing section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import JobListing from "../components/JobListing";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <JobListing />
     </div>
   );
 };
