cfba681fe13dab34464e995a680b9e9d417847e3
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-17 00:21:58 +0530

    ..

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -17,4 +17,18 @@ export const getJobs = async (req, res) => {
 };
 
 // Get a single job by id
-export const getJobById = async (req, res) => {};
+export const getJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const job = await Job.findById(id).populate({
+      path: "companyId",
+      select: "-password",
+    });
+    if (!job) {
+      return res.json({ success: false, message: "Job not found" });
+    }
+    res.json({ success: true, job });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
