9c07639ca8bae73d91584efe84cbed0987ce716d
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-16 21:23:31 +0530

    Done with the company login feature, now working on post new job feature

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -51,7 +51,28 @@ export const registerCompany = async (req, res) => {
 };
 
 // Company Login
-export const loginCompany = async (req, res) => {};
+export const loginCompany = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const company = await Company.findOne({ email });
+    if (bcrypt.compare(password, company.password)) {
+      res.json({
+        success: true,
+        company: {
+          _id: company._id,
+          name: company.name,
+          email: company.email,
+          image: company.image,
+        },
+        token: generateToken(company._id),
+      });
+    } else {
+      res.json({ success: false, message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Get company data
 export const getCompanyData = async (req, res) => {};
